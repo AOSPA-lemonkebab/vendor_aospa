8975dbb0781263a5ff8419117b76ac039cc95dd2
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-02-01 12:55:08 +0530

    Fix RMX1851
    Change-Id: I909e78d64c6e10348170eb12c20cbb3061383185

--- products/RMX1851/aospa_RMX1851.mk
+++ products/RMX1851/aospa_RMX1851.mk
@@ -5,32 +5,30 @@
 #
 
 ifeq (aospa_RMX1851,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the custom device configuration.
+$(call inherit-product, device/realme/RMX1851/device.mk)
+
 # Inherit from the AOSPA configuration.
 $(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
-# Inherit from RMX1851 device.
-$(call inherit-product, device/realme/RMX1851/device.mk)
-
-# Inherit qcom common stuff
-$(call inherit-product, device/qcom/common/common.mk)
-
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-
-# PixelBlaster
-BLASTER_BUILD_TYPE := OFFICIAL
-
-# Device identifier. This must come after all inclusions.
-PRODUCT_DEVICE := RMX1851
-PRODUCT_NAME := aospa_RMX1851
 PRODUCT_BRAND := Realme
-PRODUCT_MODEL := Realme 3 Pro
+PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := Realme
+PRODUCT_MODEL := RMX1851
+PRODUCT_NAME := aospa_RMX1851
 
-PRODUCT_GMS_CLIENTID_BASE := android-realme
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
+# Override device name for Play Store.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="RMX1851" \
-    TARGET_DEVICE="RMX1851"
+    PRODUCT_DEVICE=RMX1851
+
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
 
 endif
